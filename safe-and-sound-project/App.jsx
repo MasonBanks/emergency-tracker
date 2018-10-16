48906669e40a79ceea0915db6c847c3849d661d4
Novik71 <vpearcesmith@gmail.com>
2018-10-16 19:56:54 +0100

    keep calm and commit

--- safe-and-sound-project/App.jsx
+++ safe-and-sound-project/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-wgwpogjwgjjwgjwrg;
-
 export default class App extends React.Component {
   render() {
     return (
