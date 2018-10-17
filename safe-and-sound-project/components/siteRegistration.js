839245d2318ac7944f4a82539d5131deaa89ed46
Novik71 <vpearcesmith@gmail.com>
2018-10-17 12:26:34 +0100

    test commit

---
+++ safe-and-sound-project/components/siteRegistration.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
+import { config } from '../config/firebase-config';
+
+firebase.initializeApp(config);
+
+const { database } = firebase;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default class SiteRegistration extends React.Component {
+
+  writeNewSite = (userId, siteName, address, safeZone, buildingZone) => {
+    database().ref(`sites/${userId}`).set({
+      siteName,
+      admin: userId,
+      address,
+      safeZone,
+      buildingZone,
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hi</Text>
+        {this.writeNewSite('test user', 'fed house', 'fed street', ' testsafezone', 'testbuildingzone')}
+      </View>
+    );
+  }
+
+}
+
+
