fd1c3f070007d3f0024c09ed9a62e96ecf5e942d
Novik71 <vpearcesmith@gmail.com>
2018-10-17 11:01:13 +0100

    git ignore package-lock.json

---
+++ safe-and-sound-project/components/siteRegistration.js
@@ -0,0 +1,14 @@
+// import firebase from 'firebase';
+
+// const { database } = firebase;
+
+// // function createSite(userId, siteName, address, safeZone, buildingZone) {
+// //   database().ref(`sites/${userId}`).set({
+// //     siteName,
+// //     admin: userId,
+// //     address: '',
+// //     safeZone: null,
+// //     buildingZone: null,
+// //   });
+// // }
+
