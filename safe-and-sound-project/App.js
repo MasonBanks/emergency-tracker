7601a7bed6a0b18543ef495292cf69a26c071f53
Mason Banks <YokiStation@Masons-MacBook-Pro.local>
2018-10-18 11:55:54 +0100

    add route templates with animation

--- safe-and-sound-project/App.js
+++ safe-and-sound-project/App.js
@@ -1,66 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 import firebase from 'firebase';
-import { AppLoading, StatusBar } from 'expo';
 import { config } from './config/firebase-config';
-
+import { Provider, initialState } from './src/ContextStore';
+import Routes from './src/Routes';
 
 firebase.initializeApp(config);
 
+
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState,
+      setAuth: this.setAuth,
+    };
   }
+
+  setAuth = (authenticated) => {
+    this.setState((oldState) => ({
+      auth: {
+        ...oldState.auth,
+        authenticated,
+      },
+    }));
+  };
+
   render() {
-    if (!this.state.isLoadingComplete) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      )
-    } else {
-      return (
-        <View style={styles.container}>
-          {/* {<StatusBar barStyle="default" />} */}
-          <Text>Hello!</Text>
-        </View>
-      );
-    }
-  }
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/safensound2.png')
-      ])
-    ])
-  }
-  _handleLoadingError = error => {
-    console.log(error)
-  }
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true })
+    return (
+      <Provider value={this.state}>
+        <Routes />
+      </Provider>
+    );
   }
 }
-
-const { width, height } = Dimensions.get('window')
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  imageFormat: {
-    width: 200,
-    height: 180,
-    resizeMode: 'contain'
-  },
-  loadingTitle: {
-    height: '25%',
-    width: '50%',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
---
+++ safe-and-sound-project/src/Routes.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import { View } from 'react-native';
+import EasyRouter from 'react-native-easy-router';
+
+import { Consumer } from './ContextStore';
+
+import Drawer from './components/Drawer';
+import Sidenav from './components/Sidenav';
+import Tabs from './components/Tabs';
+
+import Intro from './screens/unauth/Intro';
+import Login from './screens/unauth/Login';
+import Home from './screens/auth/Home';
+import Profile from './screens/auth/Profile';
+import Settings from './screens/auth/Settings';
+
+const animations = {
+  effect: [
+    {
+      opacity: 0,
+      transform: [{ scale: 0 }],
+    },
+    {
+      opacity: 1,
+      transform: [{ scale: 1 }]
+    },
+    true,
+  ],
+};
+
+class Routes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      router: undefined,
+      animation: undefined,
+      from: undefined,
+      to: undefined,
+    };
+    this.drawer = React.createRef();
+  }
+
+  onStackChange = (stack) => {
+    this.setState({
+      animation: undefined,
+      from: undefined,
+      to: (stack[stack.length - 1] || {}).route,
+    });
+  };
+
+  onBeforeStackChange = (animation, fromStack, toStack) => {
+    const from = (fromStack[fromStack.length - 1] || {}).route;
+    const to = (toStack[toStack.length - 1] || {}).route;
+    this.setState({ animation, from, to });
+  };
+
+  setRouter = (router) => {
+    this.setState({ router });
+  };
+
+  closeDrawer = () => {
+    if (this.drawer.current.closeDrawer) {
+      this.drawer.current.closeDrawer();
+    }
+  };
+
+  openDrawer = () => {
+    if (this.drawer.current.openDrawer) {
+      this.drawer.current.openDrawer();
+    }
+  };
+
+  render() {
+    return (
+      <Consumer>
+        {({ setAuth, auth: { authenticated } }) => (
+          <View style={{ backgroundColor: 'black', flex: 1 }}>
+            {!authenticated && (
+              <EasyRouter
+                routes={{
+                  Intro,
+                  Login,
+                }}
+                initialRoute="Intro"
+                animations={animations}
+                onStackChange={this.onStackChange}
+                onBeforeStackChange={this.onBeforeStackChange}
+                router={(router) => {
+                  this.setRouter(router);
+                }}
+              />
+            )}
+
+            {authenticated && (
+              <Drawer
+                renderNavigationView={() => (
+                  <Sidenav setAuth={setAuth} router={this.state.router} closeDrawer={this.closeDrawer} />
+                )}
+                ref={this.drawer}>
+                <Tabs
+                  router={this.state.router}
+                  openDrawer={this.openDrawer}
+                  from={this.state.from}
+                  to={this.state.to}
+                  transition={this.state.animation}>
+                  <EasyRouter
+                    routes={{
+                      Home,
+                      Profile,
+                      Settings,
+                    }}
+                    initialRoute="Home"
+                    animations={animations}
+                    onStackChange={this.onStackChange}
+                    onBeforeStackChange={this.onBeforeStackChange}
+                    router={(router) => {
+                      this.setRouter(router);
+                    }}
+                  />
+                </Tabs>
+              </Drawer>
+            )}
+          </View>
+        )}
+      </Consumer>
+    );
+  }
+}
+
+export default Routes;
