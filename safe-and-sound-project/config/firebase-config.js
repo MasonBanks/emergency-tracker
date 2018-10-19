73f0236926bbafa8af28973c0b04faaa12a6d199
Novik71 <vpearcesmith@gmail.com>
2018-10-19 12:27:13 +0100

    migrate firebase-admin functions to backend

--- safe-and-sound-project/config/firebase-config.js
+++ safe-and-sound-project/config/firebase-config.js
@@ -1,14 +1,6 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
-
 exports.config = {
   projectId: 'safe-and-sound-app-fbe3e',
   databaseURL: 'https://safe-and-sound-app-fbe3e.firebaseio.com/',
   storageBucket: 'gs://safe-and-sound-app-fbe3e.appspot.com',
   apiKey: 'AIzaSyBYE8Z-jNX8NArzBD1mPQHZorfQstPSuRg',
-};
-
-exports.adminConfig = {
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://safe-and-sound-app-fbe3e.firebaseio.com',
 };
