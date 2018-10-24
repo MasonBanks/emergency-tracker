da17f412805ddb710469b0339d2305d472fc5d36
Mason Banks <YokiStation@Masons-MacBook-Pro.local>
2018-10-24 21:40:37 +0100

    format changes

--- safe-and-sound-project/api.js
+++ safe-and-sound-project/api.js
@@ -5,67 +5,64 @@ const { database } = firebase;
 firebase.initializeApp(config);
 
 // these two functions will need to change to accept a userID
-exports.enterBuilding = bool => {
+exports.enterBuilding = (bool) => {
   database()
     .ref('/users/0')
     .update({ inBuilding: bool });
 };
 
 // these two functions will need to change to accept a userID
-exports.enterSafeZone = bool => {
+exports.enterSafeZone = (bool) => {
   database()
     .ref('/users/0')
     .update({ inSafeZone: bool });
 };
 
-exports.createUser = (fname, lName, email, password) =>
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then(({ user }) => {
-      const { uid } = user;
-      const newUser = {
-        uid,
-        fname,
-        lName,
-        email,
-        inBuilding: false,
-        inSafeZone: false,
-        isAdmin: false,
-        isFirstAider: false
-      };
-      return database()
-        .ref(`/users/${uid}`)
-        .set(newUser)
-        .then(() =>
-          database()
-            .ref('/users')
-            .orderByKey()
-            .equalTo(uid)
-            .once('value')
-            .then(data => {
-              console.log(data.val(), '<<< User added to realtime db');
-              return data;
-            })
-            .catch(err => alert(err))
-        )
-        .catch(error => {
-          console.log(error);
-          if (error.code === 'auth/weak-password') {
-            alert('The password is too weak.');
-          } else {
-            console.log(error.message);
-          }
-        });
-    });
+exports.createUser = (fname, lName, email, password) => firebase
+  .auth()
+  .createUserWithEmailAndPassword(email, password)
+  .then(({ user }) => {
+    const { uid } = user;
+    const newUser = {
+      uid,
+      fname,
+      lName,
+      email,
+      inBuilding: false,
+      inSafeZone: false,
+      isAdmin: false,
+      isFirstAider: false,
+    };
+    return database()
+      .ref(`/users/${uid}`)
+      .set(newUser)
+      .then(() => database()
+        .ref('/users')
+        .orderByKey()
+        .equalTo(uid)
+        .once('value')
+        .then((data) => {
+          console.log(data.val(), '<<< User added to realtime db');
+          return data;
+        })
+        .catch(err => alert(err)))
+      .catch((error) => {
+        console.log(error);
+        if (error.code === 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          console.log(error.message);
+        }
+      });
+  });
 
-exports.getUserById = uid => {
+exports.getUserById = (uid) => {
   database()
     .ref('/users')
     .orderByKey()
     .equalTo(uid)
     .once('value')
-    .then(data => {
+    .then((data) => {
       if (data) {
         console.log(data.val());
         return data;
@@ -75,30 +72,29 @@ exports.getUserById = uid => {
     .catch(err => alert(err));
 };
 
-exports.login = (email, password) =>
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(({ user }) => {
-      const { uid } = user;
-      return database()
-        .ref('/users')
-        .orderByKey()
-        .equalTo(uid)
-        .once('value')
-        .then(data => {
-          if (data) {
-            return data;
-          }
-        })
-        .catch(err => alert(err));
-    });
+exports.login = (email, password) => firebase
+  .auth()
+  .signInWithEmailAndPassword(email, password)
+  .then(({ user }) => {
+    const { uid } = user;
+    return database()
+      .ref('/users')
+      .orderByKey()
+      .equalTo(uid)
+      .once('value')
+      .then((data) => {
+        if (data) {
+          return data;
+        }
+      })
+      .catch(err => alert(err));
+  });
 
-exports.toggleAdminStatus = uid => {
+exports.toggleAdminStatus = (uid) => {
   database()
     .ref(`/users/${uid}/isAdmin`)
     .once('value')
-    .then(data => {
+    .then((data) => {
       const currentStatus = data.val();
       database()
         .ref(`/users/${uid}`)
@@ -106,11 +102,11 @@ exports.toggleAdminStatus = uid => {
     });
 };
 
-exports.toggleFirstAiderStatus = uid => {
+exports.toggleFirstAiderStatus = (uid) => {
   database()
     .ref(`/users/${uid}/isFirstAider`)
     .once('value')
-    .then(data => {
+    .then((data) => {
       const currentStatus = data.val();
       database()
         .ref(`/users/${uid}`)
@@ -118,20 +114,19 @@ exports.toggleFirstAiderStatus = uid => {
     });
 };
 
-exports.emergencyStatusListener = () =>
-  database()
-    .ref('/site')
-    .child('isEmergency')
-    .on('value', snapshot => {
-      console.log(`current status: ${snapshot.val()}`);
-    });
+exports.emergencyStatusListener = () => database()
+  .ref('/site')
+  .child('isEmergency')
+  .on('value', (snapshot) => {
+    console.log(`current status: ${snapshot.val()}`);
+  });
 
 exports.toggleEmergencyStatus = () => {
   database()
     .ref('/site')
     .child('isEmergency')
     .once('value')
-    .then(data => {
+    .then((data) => {
       console.log(`current emergency status: ${data.val()}`);
       const currentStatus = data.val();
       database()
@@ -142,7 +137,7 @@ exports.toggleEmergencyStatus = () => {
             .ref('/site')
             .child('isEmergency')
             .once('value')
-            .then(newData => {
+            .then((newData) => {
               console.log(`Emergency status set to ${newData.val()}`);
             });
         });
@@ -154,7 +149,7 @@ exports.createNewEvacuation = (adminId, startTime) => {
     .ref('users')
     .orderByChild('inBuilding')
     .equalTo(true)
-    .once('value', snapshot => {
+    .once('value', (snapshot) => {
       const inBuildingUsers = snapshot.val();
       database()
         .ref(`evacuations/${startTime}`)
@@ -162,99 +157,94 @@ exports.createNewEvacuation = (adminId, startTime) => {
           adminId,
           startTime,
           finishTime: null,
-          inBuildingUsers
+          inBuildingUsers,
         });
     });
 };
 
-exports.getEvacList = adminId => {
+exports.getEvacList = (adminId) => {
   database()
     .ref('evacuations')
     .orderByChild('adminId')
     .equalTo(adminId)
     .once('value')
-    .then(data => {
+    .then((data) => {
       mostRecentStamp = Object.keys(data.val()).sort((a, b) => b - a)[0];
       database()
         .ref(`evacuations/${mostRecentStamp}/inBuildingUsers`)
         .once('value')
-        .then(users => {
+        .then((users) => {
           const evacList = Object.values(users.val());
           return evacList;
         });
     });
 };
 
-exports.endCurrentEvacuation = adminId => {
+exports.endCurrentEvacuation = (adminId) => {
   database()
     .ref('evacuations')
     .orderByChild('adminId')
     .equalTo(adminId)
     .once('value')
-    .then(data => {
+    .then((data) => {
       mostRecentStamp = Object.keys(data.val()).sort((a, b) => b - a)[0];
       database()
         .ref(`evacuations/${mostRecentStamp}`)
         .update({
-          finishTime: Date.now()
+          finishTime: Date.now(),
         });
     });
 };
 
-exports.getSafeZone = () =>
-  database()
-    .ref('/site/safeZone')
-    .once('value')
-    .then(data => data);
+exports.getSafeZone = () => database()
+  .ref('/site/safeZone')
+  .once('value')
+  .then(data => data);
 
-exports.getBuilding = () =>
-  database()
-    .ref('/site/building')
-    .once('value')
-    .then(data => data);
+exports.getBuilding = () => database()
+  .ref('/site/building')
+  .once('value')
+  .then(data => data);
 
-exports.saveSafeZone = (Zone, zoneName) =>
-  database()
-    .ref('/site')
-    .update({
-      [zoneName]: Zone
-    });
+exports.saveSafeZone = (Zone, zoneName) => database()
+  .ref('/site')
+  .update({
+    [zoneName]: Zone,
+  });
 
-exports.userInBuilding = uid => {
+exports.userInBuilding = (uid) => {
   database()
     .ref(`/users/${uid}`)
     .update({ inBuilding: true });
 };
 
-exports.userExitBuilding = uid => {
+exports.userExitBuilding = (uid) => {
   database()
     .ref(`/users/${uid}`)
     .update({ inBuilding: false });
 };
 
-exports.userInSafeZone = uid => {
+exports.userInSafeZone = (uid) => {
   database()
     .ref(`/users/${uid}`)
     .update({ inSafeZone: true });
 };
 
-exports.userExitSafeZone = uid => {
+exports.userExitSafeZone = (uid) => {
   database()
     .ref(`/users/${uid}`)
     .update({ inSafeZone: false });
 };
 
-exports.getAllUsers = () =>
-  database()
-    .ref('/users')
-    .once('value')
-    .then(userData => userData.val());
+exports.getAllUsers = () => database()
+  .ref('/users')
+  .once('value')
+  .then(userData => userData.val());
 
-exports.updateUser = (uid, entriesToUpdateObj) =>
-  database()
-    .ref(`/users/${uid}`)
-    .update(entriesToUpdateObj)
-    .then(updatedData => updatedData.val());
+exports.updateUser = (uid, entriesToUpdateObj) => database()
+  .ref(`/users/${uid}`)
+  .update(entriesToUpdateObj)
+  .then(updatedData => updatedData.val());
 
 // exports.userInBuilding = (uid) => {
 //   console.log(uid);
