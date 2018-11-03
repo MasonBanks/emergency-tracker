f9f5aa75973b01ec16bc9eb73e4b69dce9684e6e
Novik71 <vpearcesmith@gmail.com>
2018-11-03 16:42:02 +0000

    Removed backend directory from main repo, to be added as a submodule

--- server-safe-and-sound/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const { PORT = 9090 } = process.env;
-
-app.listen(PORT, (err) => {
-  if (err) console.log('Server failed to connect to host');
-  else console.log(`listening on port ${PORT}`);
-});
