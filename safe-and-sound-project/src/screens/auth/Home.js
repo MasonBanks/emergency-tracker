821e2064434bf155c6cc7bdbaeeb68c175bad405
Novik71 <vpearcesmith@gmail.com>
2018-11-03 15:47:01 +0000

    stash pop

--- safe-and-sound-project/src/screens/auth/Home.js
+++ safe-and-sound-project/src/screens/auth/Home.js
@@ -29,138 +29,58 @@ const styles = StyleSheet.create({
     backgroundColor: 'green',
     width: '40%',
     height: 40,
+    margin: 8,
+    paddingTop: 7,
   },
   redButton: {
     backgroundColor: 'red',
     width: '40%',
     height: 40,
+    margin: 8,
+    paddingTop: 7,
   },
 });
 
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      token: null,
-      uid: null,
-    };
-    this.router = this.props.router;
-  }
-
-  componentWillMount() {
-    if (!this.state.uid) {
-      return this.setState({
-        uid: state.auth.authenticated,
-      });
-    }
-  }
-
-  componentDidMount() {
-    console.log('Component did mount! These are the props:', this.props, 'This is the state: ', this.state);
-    this.getPushTokenAsync();
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('Should component update', this.state.uid, nextState.uid);
-    if (this.state.uid !== nextState.uid) return false;
-    return true;
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    console.log('Component will update!', nextProps, nextState);
-    this.registerForPushNotificationsAsync()
-      .then();
-  }
-
-  getPushTokenAsync = async () => {
-    const { status: existingStatus } = await Permissions.getAsync(
-      Permissions.NOTIFICATIONS,
-    );
-    let finalStatus = existingStatus;
-
-    if (existingStatus !== 'granted') {
-      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-      finalStatus = status;
-    }
-
-    if (finalStatus !== 'granted') {
-      return;
-    }
-
-    const token = await Notifications.getExpoPushTokenAsync();
-
-    return this.setState({
-      token,
-    });
-  }
-
-  registerForPushNotificationsAsync() {
-    // POST the token to your backend server from where you can retrieve it to send push notifications.
-    return fetch(`PUSH_ENDPOINT/${this.state.uid}/${this.state.token}`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        token: {
-          value: this.state.token,
-        },
-        user: {
-          uid: this.state.uid,
-        },
-      }),
-    });
-  }
-
-  render() {
-    const { router } = this.props;
-    return (
-      <GlobalContext.Consumer>
-        {({ state, setMode }) => (
-          <Screen
-            backgroundColor={state.mode.emergency ? '#F05555' : '#4ec3c9'}
-            title="Home"
-          >
-            {!this.state.uid && (
-              this.setState({
-                uid: state.auth.authenticated,
-              })
-            )}
-            {state.mode.emergency ? (
-              <View style={styles.outterContainer}>
-                <EmergencyUserMap />
-                <View style={styles.buttonContainer}>
-                  <Button
-                    style={styles.greenButton}
-                    onPress={() => {
-                      api.updateUser(state.auth.authenticated, {
-                        markedSafe: true,
-                        markedInDanger: false,
-                      });
-                      addMeToEvacSafeList(state.auth.authenticated);
-                      alert('You have been marked safe. Please wait patiently within the safe zone until given further instructions from the fire warden or emergency services. Thank you!');
-                    }}
-                    text="Mark Safe"
-                  />
-                  <Button
-                    style={styles.redButton}
-                    onPress={() => {
-                      updateUser(state.auth.authenticated, {
-                        markedInDanger: true,
-                      });
-                      alert('An alert has been signaled to the fire warden and your location will be relayed to the emergency services. Please remain calm, help is on the way!');
-                    }}
-                    text="I am in danger!"
-                  />
-                </View>
-              </View>
-            ) : (
-              <FireEscapeMap />
-            )}
-          </Screen>
-        )}
-      </GlobalContext.Consumer>
-    );
-  }
-}
+export default (Home = ({ router }) => (
+  <GlobalContext.Consumer>
+    {({ state }) => (
+      <Screen
+        backgroundColor={state.mode.emergency ? '#F05555' : '#4ec3c9'}
+        title="Home"
+      >
+        {state.mode.emergency ? (
+          <View style={styles.outterContainer}>
+            <EmergencyUserMap />
+            <View style={styles.buttonContainer}>
+              <Button
+                style={styles.greenButton}
+                onPress={() => {
+                  api.updateUser(state.auth.authenticated, {
+                    markedSafe: true,
+                    markedInDanger: false,
+                  });
+                  api.addMeToEvacSafeList(state.auth.authenticated);
+                  alert('You have been marked safe. Please wait patiently within the safe zone until given further instructions from the fire warden or emergency services. Thank you!');
+                }}
+                text="I'm Safe"
+              />
+              <Button
+                style={styles.redButton}
+                onPress={() => {
+                  api.updateUser(state.auth.authenticated, {
+                    markedSafe: false,
+                    markedInDanger: true,
+                  });
+                  alert('An alert has been signaled to the fire warden and your location will be relayed to the emergency services. Please remain calm, help is on the way!');
+                }}
+                text="I'm in Danger"
+              />
+            </View>
+          </View>
+        ) : (
+            <FireEscapeMap />
+          )}
+      </Screen>
+    )}
+  </GlobalContext.Consumer>
+));
