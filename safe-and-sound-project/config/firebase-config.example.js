857a50d7c249be043b8a801b0e9ea1d4da839d9e
Novik71 <vpearcesmith@gmail.com>
2018-11-17 17:26:28 +0000

    add firebase-config.example.js

---
+++ safe-and-sound-project/config/firebase-config.example.js
@@ -0,0 +1,6 @@
+exports.config = {
+  projectId: 'EXAMPLE_DATABASE_ID',
+  databaseURL: 'https://EXAMPLE_DATABASE_URL.firebaseio.com/',
+  storageBucket: 'gs://EXAMPLE_STORAGE_BUCKET.appspot.com',
+  apiKey: 'EXAMPLE_APIKEY',
+};
